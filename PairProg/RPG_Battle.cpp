d7ecf980ab769d8e436a924d0df0a462fd303060
IanE-23 <ianmengel@outlook.com>
2025-03-28 16:10:26 -0500

    Changed to use multiple functions
    Added treasure as a reward for winning. (Supposed to be random but always seems to be 1)

--- PairProg/RPG_Battle.cpp
+++ PairProg/RPG_Battle.cpp
@@ -1,18 +1,50 @@
-/*welcome the player $$
-set up the game
-int health = 10, attack, block, turns = 0 $$
-seed the random number generator $$
-start the loop
-add 1 to turns $$
-start the encounter
-randomly generate numbers for attack(range = 0 - 4) and block(range = 0 - 4)
-if block is greater or equal to attack, successful block $$
-otherwise, subtract attack value from health. $$
-ask the player if they would like to keep adventuring. if they don't, break out of the loop.
-keep looping while health is greater than zero and fewer than 4 turns have happened
+/*Story()
 
-if health is greater than 0, congratulate player
-otherwise, tell the player they're dead.*/
+- return type is void
+
+AskYesNo()
+
+- One string argument named "question"
+
+- return type is bool
+
+RollDie()
+
+- one int parameter named "sides" with a default value of 6
+
+- return type is int
+
+Ending()
+
+- return type is void
+
+Adventure()
+
+- return type is void or bool
+
+- calls rollDie() for attack, block, & treasure
+
+- tells the player their health after the encounter
+
+
+
+obviously main()
+
+- calls story,
+
+-has a loop for adventure,
+
+-checks for health
+
+- calls ending()
+
+
+
+Create the following global variables :
+
+-health
+
+- totalTreasure*/
 
 #include <iostream>		// this sends and recieve text from the console
 #include <string>		// allows the use of string variables
@@ -21,95 +53,126 @@ otherwise, tell the player they're dead.*/
 
 using namespace std;
 
-int main()
-{
+// global variables
+
+int playerHP = 30;
+int enemyHP = 10;
+int pAttack, pBlock = 0;
+int eAttack, eBlock = 0;
+int totalTreasure = 0;
+
+void Story() {
+    cout << "You heard news from the tavern keeper that this cave holds treasure beyond compare. \n But, the cave is filled with monsters with no extent.\n";
+    string input = "";
+    cout << "Will you try your hand to get some of that treasure? (y/n)\n";
+    getline(cin, input);
+}
+
+int RollDie(int sides = 6) {
+
     srand(time(0));
 
-    // block of integer variables
-    int playerHP = 10;
-    int enemyHP = 10;
-    int pAttack, pBlock = 0;
-    int eAttack, eBlock = 0;
-    int turn = 0;
-    int encounter = 0;
 
-    // block of string variables
+    sides = (rand() % 20) + 1;
+
+    // used for calculating attack and treasure gain
+    pAttack = (rand() % 10) + 1;
+    eAttack = (rand() % 6) + 1;
+
+    // blocking variables
+    pBlock = (rand() % 6) + 1;
+    eBlock = (rand() % 6) + 1;
+
+    return 0;
+
+}
+
+bool AskYesNo(string question = "y/n") {
     string input = "";
+    cout << "Will you continue forth? (y/n)\n";
+    getline(cin, input);
 
-    // Welcomes the player to the game
-     cout << "Welcome to RPG Battle Simulator\n";
-     cout << "Rules of the Game:\n";
-     cout << "\t4 turns per encounter\n \tGreater block stops attacks\n";
-     cout << "\tPlayer will restore 3 HP with each successful encounter\n";
+    if (input == "n") {
+        return false;
+    }
+    else if (input == "y"){
+        return true;
+    }
+    else {
+        cout << "Please select 'y' for yes or 'n' for no.\n";
+    }
+}
 
-    while (playerHP >= 0)        // proceeds the game
+bool Adventure() {
+
+    RollDie();
+    cout << "Player Attacks!\n";
+    if (pAttack > eBlock)
     {
-        turn += 1;
-        
-        // attacking variables
-        pAttack = (rand() % 4) + 1;
-        eAttack = (rand() % 4) + 1;
-        
-        // blocking variables
-        pBlock = (rand() % 4) + 1;
-        eBlock = (rand() % 4) + 1;
-        
-        // player's attack
-        cout << "Player Attacks!\n";
-        if (pAttack > eBlock) 
-        {
-            int pDamage = pAttack - eBlock;
-            enemyHP -= pDamage;
-            cout << "Current enemy Health: " << enemyHP << "\n";
-        }
-        else 
-        {
-            cout << "Current Enemy Health: " << enemyHP << "\n";
-        }
+        int pDamage = pAttack - eBlock;
+        enemyHP -= pDamage;
+        cout << "Current enemy Health: " << enemyHP << "\n";
+    }
+    else
+    {
+        cout << "Current Enemy Health: " << enemyHP << "\n";
+    }
 
-        // enemey's attack
-        cout << "Enemy Attacks!\n";
-        if (eAttack > pBlock) 
-        {
-            int eDamage = eAttack - pBlock;
-            playerHP -= eDamage;
-            cout << "Current Player Health: " << playerHP << "\n";
-        }
-        else 
-        {
-            cout << "Current Player Health: " << playerHP << "\n";
-        }
-
-        if (enemyHP == 0) 
-        {
-            cout << "Continue? (y/n)\n";
-            cin >> input;
-            encounter += 1;
-            if (input == "n") 
-            {
-                cout << "Thanks for playing!\n";
-            }
-            enemyHP = 10;
-            playerHP += 3;
-        } 
-        
-        // Comment out for unlimited turns
-        if (turn == 4)              // adds a turn limit to encounters
-        {
-            cout << "You ran out of turns!\n";
-            cout << "Thanks for playing!\n";
-            break;
-
-        }
+    // enemey's attack
+    cout << "Enemy Attacks!\n";
+    if (eAttack > pBlock)
+    {
+        int eDamage = eAttack - pBlock;
+        playerHP -= eDamage;
+        cout << "Current Player Health: " << playerHP << "\n";
+    }
+    else
+    {
+        cout << "Current Player Health: " << playerHP << "\n";
+    }
     
-        if (playerHP <= 0)          // ends the game
-        {
-            cout << "You have died.\n";
-            cout << "You made it " << encounter << " stages far!\n";
+    if (enemyHP <= 0)
+    {
+        enemyHP += 10;
+
+        cout << "You gained " << RollDie(rand() % 20) + 1 << " pieces of treasure!\n";
+
+        totalTreasure += RollDie(rand() % 20) + 1;
+
+        cout << "You now have " << playerHP << " health and " << totalTreasure << " pieces of treasure!\n";
+    }
+    return true;
+}
+
+void Ending() {
+    if (playerHP <= 0) {
+        cout << "You have died due to your greed and lost all of your precious treasure.";
+    }
+    else if (playerHP <= 5 && playerHP > 0) {
+        cout << "You barely made it out with " << totalTreasure << ". You should get to the closest temple to rest though.";
+    }
+    else if (playerHP > 30) {
+        cout << "You cheated, your treasure turns to ashes and is whisked away in the wind for someone else to recieve.";
+    }
+    else {
+        cout << "You made out with all " << totalTreasure << " piece(s) of treasure.";
+    }
+}
+ 
+int main()
+{
+    cout << "Rules of the Game:\n";
+    cout << "\tGreater block stops attacks\n";
+    Story();
+    while(Adventure() == true)  {
+        if (playerHP <= 0) {
+            Ending();
+            break;
+        }
+
+        if (AskYesNo() == false) {
+            Ending();
             break;
         }
     }
-    
-   
-
 }
